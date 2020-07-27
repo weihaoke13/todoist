ae6d18aa685d33a4cb1af5f1fdbd54456411409f
weihaoke13 <weihaoke13@gmail.com>
2020-07-27 16:04:52 -0700

    fix app.scss + taskDate + sidebar attribute

--- src/components/ProjectOverlay.js
+++ src/components/ProjectOverlay.js
@@ -1,38 +1,49 @@
+//github
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { useProjectsValue } from '../context';
 
 export const ProjectOverlay = ({
-    setProject,
-    showProjectOverlay,
-    setShowProjectOverlay,
+  setProject,
+  showProjectOverlay,
+  setShowProjectOverlay,
 }) => {
-    const { projects } = useProjectsValue();
+  const { projects } = useProjectsValue();
 
-    return (
-        projects &&
-        showProjectOverlay && (
-            <div className="project-overlay" data-testid="project-overlay">
-                <ul className="project-overlay__list">   
-                        {projects.map((project) => (
-                            <li key={project.projectid}>
-                            <div
-                                data-testid="project-overlay-action"
-                                onClick={() => {
-                                    setProject(project.projectid);
-                                    setShowProjectOverlay(false);
-                                }}
-                                
-                            >
-                                {project.name}
-                            </div>
-                            </li>
-                        ))}
-                </ul>
-            </div>
-
-        )
-
+  return (
+    projects &&
+    showProjectOverlay && (
+      <div className="project-overlay" data-testid="project-overlay">
+        <ul className="project-overlay__list">
+          {projects.map((project) => (
+            <li key={project.projectId}>
+              <div
+                data-testid="project-overlay-action"
+                onClick={() => {
+                  setProject(project.projectId);
+                  setShowProjectOverlay(false);
+                }}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter') {
+                    setProject(project.projectId);
+                    setShowProjectOverlay(false);
+                  }
+                }}
+                role="button"
+                tabIndex={0}
+                aria-label="Select the task project"
+              >
+                {project.name}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     )
+  );
+};
 
-}
+ProjectOverlay.propTypes = {
+  projects: PropTypes.array,
+};
+
